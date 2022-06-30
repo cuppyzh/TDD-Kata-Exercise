0081ee6ee9b36414e68665ebdb54c4a18e6a20dc
Yusuf Anugrah Putra Aditama <yusuf.anugrah@xtremax.com>
2022-06-30 15:25:37 +0700

    [TDD-Kata-3]

---
+++ Kata1/Cuppyzh.Exercise.TDD.Kata1.Tests/PasswordVerificationTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Cuppyzh.Exercise.TDD.Kata1.Tests
+{
+    public class PasswordVerificationTests
+    {
+        private readonly PasswordVerification _passwordVerification = new PasswordVerification();
+
+        public PasswordVerificationTests()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("asdf1asdA")]
+        [InlineData("12345678aZ")]
+        [InlineData("12345678aa")]
+        [InlineData("ABCABCzz")]
+        public void PasswordVerification_ValidResult(string password)
+        {
+            var result = _passwordVerification.Verification(password);
+
+            result.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(null, PasswordVerificationUtils.PASSWORD_NULL_ERROR)]
+        [InlineData("", PasswordVerificationUtils.PASSWORD_LENGTH_ERROR)]
+        [InlineData("ASDFASDFASDF", PasswordVerificationUtils.PASSWORD_LOWERCASE_ERROR)]
+        [InlineData("ASDFASDFASDF", PasswordVerificationUtils.PASSWORD_NUMBER_ERROR)]
+        [InlineData("123456789", PasswordVerificationUtils.PASSWORD_LOWERCASE_ERROR)]
+        [InlineData("123456789", PasswordVerificationUtils.PASSWORD_UPPERCASE_ERROR)]
+        [InlineData("a1D", PasswordVerificationUtils.PASSWORD_LENGTH_ERROR)]
+        public void PasswordVerification_InvalidResult(string password, string expectedError)
+        {
+            Action result = () => _passwordVerification.Verification(password);
+
+            result.Should().Throw<Exception>().Where(message => message.Message.Contains(expectedError));
+        }
+    }
+}
---
+++ Kata1/Cuppyzh.Exercise.TDD.Kata1/PasswordVerificationUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cuppyzh.Exercise.TDD.Kata1
+{
+    public static class PasswordVerificationUtils
+    {
+        public const string PASSWORD_LENGTH_ERROR = "password should be larger than 8 chars";
+        public const string PASSWORD_NULL_ERROR = "password should not be null";
+        public const string PASSWORD_UPPERCASE_ERROR = "password should have one uppercase letter at least";
+        public const string PASSWORD_LOWERCASE_ERROR = "password should have one lowercase letter at least";
+        public const string PASSWORD_NUMBER_ERROR = "password should have one number at least";
+    }
+}
